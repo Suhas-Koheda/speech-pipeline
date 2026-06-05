605cfc851f59992a551fe694484b0006c1618c88
Unknown Haas <sharmasuhas450@gmail.com>
2026-06-05 17:23:17 +0530

    refactor: optimize audio pipeline, implement VAD, and add gitignore

--- scripts/download_audio.py
+++ scripts/download_audio.py
@@ -1,18 +1,81 @@
-from scripts.download_videos import download_audio
+from extract_metatdata import collect_metadata
 import json
+from yt_dlp import YoutubeDL
+from pathlib import Path
+import re
+
+def sanitize_name(name):
+    return re.sub(r'[<>:"/\\|?*]', "_", name)
 
 def get_audio(metadata_file_path, output_dir):
     with open(metadata_file_path, 'r') as f:
         metadata = json.load(f)
     for video in metadata:
-        if video["duration"] is None:
-            continue
+        try:
+            if video["duration"] is None:
+                continue
 
-        if video["duration"] < 300:
-            continue
+            if video["duration"] < 300:
+                continue
 
-        download_audio(
-            video["url"],
-            video["channel"],
-            "mp3"
-        )
+            download_audio(
+                video["url"],
+                video["channel"],
+                "mp3",
+                output_dir
+            )
+
+        except Exception as e:
+            print(f"Failed: {video['url']}")
+            print(e)
+
+def download_audio(video_url: str, channel_name: str, file_type: str,output_dir:str):
+    channel_name = sanitize_name(channel_name)
+    Path(f"{output_dir}/{channel_name}").mkdir(
+        parents=True,
+        exist_ok=True,
+    )
+
+    ydl_opts = {
+        "format": "bestaudio/best" if file_type == "mp3" else "best",
+        "outtmpl": f"{output_dir}/{channel_name}/%(title)s.%(ext)s",
+        "download_archive": "downloaded.txt",
+    }
+
+    if file_type == "mp3":
+        ydl_opts["postprocessors"] = [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            }
+        ]
+
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download([video_url])
+
+
+if __name__=="__main__":
+    collect_metadata(
+        "/home/ssp/UnknownHaas/speech-pipeline/links.txt"
+    )
+    get_audio(
+        "/home/ssp/UnknownHaas/speech-pipeline/data/metadata.json",
+        "/home/ssp/UnknownHaas/speech-pipeline/audio"
+    )
+# https://www.youtube.com/watch?v=9QpkWAyG-eE&t=203s&pp=ygUSdGVsYW5nYW5hIHBvZGNhc3Rz
+# https://www.youtube.com/watch?v=TdVWQ8jtZRM&t=1022s&pp=ygUSdGVsYW5nYW5hIHBvZGNhc3Rz
+# https://www.youtube.com/watch?v=d6-IFzGisYQ&pp=ygUSdGVsYW5nYW5hIHBvZGNhc3Rz
+# https://www.youtube.com/watch?v=XZqK-I-kc-Y&t=132s&pp=ygUSdGVsYW5nYW5hIHBvZGNhc3Rz0gcJCSgLAYcqIYzv
+# https://www.youtube.com/watch?v=7FbeLGB6lr0&pp=ygUSdGVsYW5nYW5hIHBvZGNhc3Rz
+# https://www.youtube.com/watch?v=mkPWFBmCpw8&pp=ygUSdGVsYW5nYW5hIHBvZGNhc3Rz
+
+
+# channels
+# https://www.youtube.com/@Telanganavillagefood/videos
+
+
+
+
+
+# standup-comedy
--- scripts/download_videos.py
+++ scripts/download_videos.py
@@ -1,6 +1,4 @@
-from pathlib import Path
 from yt_dlp import YoutubeDL
-
 
 def get_video_urls(channel_url: str):
     opts = {
@@ -25,46 +23,3 @@ def get_video_urls(channel_url: str):
     ]
 
     return channel_name, videos
-
-
-from pathlib import Path
-from yt_dlp import YoutubeDL
-
-
-def download_audio(video_url: str, channel_name: str, file_type: str):
-    Path(f"downloads/{channel_name}").mkdir(
-        parents=True,
-        exist_ok=True,
-    )
-
-    ydl_opts = {
-        "format": "bestaudio/best" if file_type == "mp3" else "best",
-        "outtmpl": f"downloads/{channel_name}/%(title)s.%(ext)s",
-    }
-
-    if file_type == "mp3":
-        ydl_opts["postprocessors"] = [
-            {
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "mp3",
-                "preferredquality": "192",
-            }
-        ]
-
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([video_url])
-        
-def download_channel(channel_url: str):
-    channel_name, video_urls = get_video_urls(channel_url)
-
-    print(f"Channel: {channel_name}")
-    print(f"Found {len(video_urls)} videos")
-
-    for idx, url in enumerate(video_urls, start=1):
-        print(f"[{idx}/{len(video_urls)}]")
-        download_audio(url, channel_name)
-
-if __name__ == "__main__":
-    download_channel(
-        "https://www.youtube.com/@Telanganavillagefood/videos"
-    )
